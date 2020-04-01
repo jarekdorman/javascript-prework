29665f1c78be4bb2ee9981096ea0948269925467
Jay Dorman <jarekdorman@gmail.com>
2020-04-01 13:50:21 -0400

    Add computerMove script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let computerMove = 'kamien';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagralem ' + computerMove +  'Jesli Twoj ruch to papier to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3) {
+  computerMove = 'nozyce';
+} 
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
+
 
